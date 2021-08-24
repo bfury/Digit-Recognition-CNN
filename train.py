b688dc1e73f1b16727a9f8f6a70925cd4a8efeaa
bfury <wssy1st@protonmail.ch>
2021-08-24 09:44:44 +0800

    update

--- train.py
+++ train.py
@@ -27,7 +27,7 @@ class CNN(object):
 
 class DataSource(object):
     def __init__(self):
-        # mnist数据集存储的位置，如何不存在将自动下载
+        # mnist数据集存储的位置，如果不存在将自动下载
         data_path = os.path.abspath(os.path.dirname(
             __file__)) + '/mnist/data_set/mnist.npz'
         (train_images, train_labels), (test_images,
